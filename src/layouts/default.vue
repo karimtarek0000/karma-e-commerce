2be17d611021263ff922e1d7a6fa0638105987bd
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-08-06 18:12:36 +0300

    Configrations

---
+++ src/layouts/default.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Default Layout</h1>
+    <slot />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Landing page</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
