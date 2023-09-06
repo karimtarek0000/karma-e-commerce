12594310c061058b964af64877cb211921503a51
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-09-06 07:03:12 +0300

    Fix scroll

--- src/components/Navbar/Mobile.vue
+++ src/components/Navbar/Mobile.vue
@@ -14,29 +14,29 @@ const links = ref([
   {
     name: "cart",
     path: "/cart",
-    icon: "cart",
+    icon: "my-cart",
   },
 ]);
 </script>
 
 <template>
-  <div class="relative h-screen overflow-hidden">
-    <nav class="navbar">
-      <NuxtLink v-for="link in links" :key="link.name" :to="link.path" class="link">
-        <ShareRenderSVG :iconName="link.icon" sizes="w-[20px]" />
-        {{ link.name }}
-      </NuxtLink>
-      <!-- For open side menu -->
-      <button type="button" @click="toggleItemHandler" class="link">
-        <ShareRenderSVG iconName="profile" sizes="w-[20px]" />
-        settings
-      </button>
-    </nav>
-    <!-- Side menu -->
+  <nav class="navbar">
+    <NuxtLink v-for="link in links" :key="link.name" :to="link.path" class="link">
+      <ShareRenderSVG :iconName="link.icon" sizes="w-[20px]" />
+      {{ link.name }}
+    </NuxtLink>
+    <!-- For open side menu -->
+    <button type="button" @click="toggleItemHandler" class="link">
+      <ShareRenderSVG iconName="profile" sizes="w-[20px]" />
+      settings
+    </button>
+  </nav>
+  <!-- Side menu -->
+  <Teleport to="body">
     <aside :class="['sidemenu', { 'sidemenu-active': itemStatus }]">
       <button @click="toggleItemHandler" type="button" class="text-2xl">&times;</button>
     </aside>
-  </div>
+  </Teleport>
 </template>
 
 <style scoped>
@@ -47,8 +47,9 @@ const links = ref([
   @apply flex flex-col items-center px-2 py-2 text-14 gap-y-1 grow;
 }
 .sidemenu {
-  @apply absolute top-0 w-[200px] transition-all duration-500 shadow-lg end-[-200px] h-screen p-2 bg-white;
+  @apply overflow-hidden absolute top-0 w-[200px] z-20 transition-all duration-500 shadow-lg end-[-200px] h-screen p-2 bg-white;
 }
+
 .sidemenu-active {
   @apply end-0;
 }
