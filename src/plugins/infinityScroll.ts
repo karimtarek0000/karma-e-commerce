c4b960ffa4e19370bf7b3980ebc996c8eebd755f
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-09-07 04:09:13 +0300

    Create plugin infinity scroll

---
+++ src/plugins/infinityScroll.ts
@@ -0,0 +1,22 @@
+import { DirectiveBinding } from "nuxt/dist/app/compat/capi";
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  const scrollFn = (binding: any, el: HTMLElement) => {
+    const { scrollTop, scrollHeight, clientHeight } = el;
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+      binding.value();
+    }
+  };
+
+  vueApp.directive("scroll", {
+    mounted(el: HTMLElement, binding: DirectiveBinding) {
+      // Add an event listener for the scroll event
+      el.addEventListener("scroll", () => scrollFn(binding, el));
+    },
+    unmounted(el: HTMLElement, binding: DirectiveBinding) {
+      // Remove the event listener when the component is unmounted
+      el.removeEventListener("scroll", () => scrollFn(binding, el));
+    },
+  });
+});
