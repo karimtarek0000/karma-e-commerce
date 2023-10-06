102cf340c9bf3af006bef900bb8d94263d5e0390
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-10-07 01:02:39 +0300

    Modal

--- src/components/Modal/Controller.vue
+++ src/components/Modal/Controller.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
-// const confirm = resolveComponent("ModalConfirm");
-// const order = resolveComponent("ModalOrder");
+const boo = ref(true);
+const confirm = resolveComponent("ModalConfirm");
+const order = resolveComponent("ModalOrder");
 </script>
 
 <template>
-  <div>
-    <!-- <Component :is="order"></Component> -->
-  </div>
+  <Transition mode="out-in">
+    <Component :is="boo ? order : confirm"></Component>
+  </Transition>
+  <!-- <button @click="boo = !boo">toggle</button> -->
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active {
+  opacity: 1;
+  animation: slideUp 1s ease-in-out forwards;
+}
+
+@keyframes slideUp {
+  from {
+    transform: translateY(-10px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
+</style>
