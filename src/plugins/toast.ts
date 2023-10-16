808672c9ee21a827c2b4270e3b9012321f16a86a
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-10-16 22:08:41 +0300

    Update toast plugin

--- src/plugins/toast.ts
+++ src/plugins/toast.ts
@@ -2,11 +2,18 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  // Prevent all previous toasts
+  const filterBeforeCreate = (toast: any, toasts: any) => (toasts.length ? toasts[0] : toast);
+
+  // Implementations
   nuxtApp.vueApp.use(Toast, {
-    timeout: 5000,
+    timeout: 2000,
     closeOnClick: true,
     draggable: true,
     hideProgressBar: true,
     closeButton: "button",
+    maxToasts: 1,
+    newestOnTop: true,
+    filterBeforeCreate,
   });
 });
