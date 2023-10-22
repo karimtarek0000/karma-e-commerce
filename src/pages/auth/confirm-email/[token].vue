768fa2c94b1c16d7bce23ce9422a6b08f6871d6d
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-10-22 03:57:45 +0300

    Confirm email

---
+++ src/pages/auth/confirm-email/[token].vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { useToast } from "vue-toastification";
+
+// ----------- Composables -----------
+const {
+  params: { token },
+} = useRoute();
+const http = useHttp();
+const toast = useToast();
+
+// ----------- API -----------
+const {
+  data,
+  error,
+  pending: loader,
+  execute,
+} = await useLazyAsyncData(() => http(`/auth/confirm/${token}`), {
+  immediate: false,
+});
+loader.value = false;
+
+// --------- functions -----------
+const sendConfirm = async () => {
+  await execute();
+
+  if (!loader.value && !error.value) {
+    toast.success(data.value.message);
+    navigateTo("/auth");
+  }
+
+  if (error.value) {
+    toast.error(error.value.message);
+  }
+};
+
+// ----------- Meta ------------
+useSeoMeta({
+  title: "Auth - confirm email",
+});
+
+// --------- Define -----------
+definePageMeta({
+  layout: "auth",
+  validate: async ({ params }) => {
+    return params.token.length > 20;
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-5 mt-20">
+    <p>Valid for only 30 minutes</p>
+    <button
+      @click="sendConfirm"
+      class="flex items-center gap-4 px-4 py-2 text-white rounded-md bg-secondary"
+    >
+      <ShareLoader v-if="loader" />
+      Verify email
+    </button>
+  </div>
+</template>
