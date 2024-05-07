f76d42745b8977bbb5c859c07f3128d637fc25b5
karim <karimtarekabdelaziz@gmail.com>
2024-05-07 23:27:09 +0300

    Feat: Add active class on category navbar

--- src/components/Category/Navbar.vue
+++ src/components/Category/Navbar.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 const http = useHttp();
-const { push } = useRouter();
 
 // ----------------- From API ---------------------
 const { data: categories } = await useAsyncData(() => http(CATEGORIES));
@@ -18,15 +17,15 @@ const overCategoryHandler = (category: Gategory): void => {
     <div class="container relative">
       <div class="wrapper">
         <div class="links">
-          <button
+          <NuxtLink
             v-for="category in allCategories"
             :key="category.name"
             class="font-bold link"
+            :to="`/all-categories/${category?._id}`"
             @mouseover="overCategoryHandler(category)"
-            @click="push(`/all-categories/${category?._id}`)"
           >
             {{ category.name }}
-          </button>
+          </NuxtLink>
 
           <!-- View the category after the user hover on it -->
           <CategoryView :category="setCategory" />
@@ -53,5 +52,10 @@ const overCategoryHandler = (category: Gategory): void => {
 .link:hover ~ .view,
 .view:hover {
   @apply translate-y-0 opacity-100 pointer-events-auto;
+}
+
+/* Active link */
+.router-link-exact-active {
+  @apply text-white bg-secondary;
 }
 </style>
