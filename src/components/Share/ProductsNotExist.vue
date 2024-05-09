507bdebb1041a1fd79b54b80a08ccb5a8eb90182
karim <karimtarekabdelaziz@gmail.com>
2024-05-09 19:52:53 +0300

    Feat: Create new component ProductNotExit

---
+++ src/components/Share/ProductsNotExist.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    <NuxtImg src="/img/cartempty.svg" class="res-image" fit="cover" />
+    <h3 class="font-bold">Not exist any product yet!</h3>
+    <NuxtLink to="/" class="not-found__button">Go to home page</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  @apply max-w-[15.5rem] min-h-[16rem] mx-auto py-4 text-center;
+}
+.not-found__button {
+  @apply block px-2 py-3 mt-4 text-white rounded-md hover:bg-secondary/95 bg-secondary;
+}
+</style>
